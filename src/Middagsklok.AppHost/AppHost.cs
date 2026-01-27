6cc80399eb060b2c3b012b50d04c1f2221228e6e
Leif Larsen <me@leiflarsen.org>
2026-01-27 16:19:03 +0100

    feat: add initial nextjs app to project

--- src/Middagsklok.AppHost/AppHost.cs
+++ src/Middagsklok.AppHost/AppHost.cs
@@ -1,3 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var apiService = builder.AddProject<Projects.Middagsklok_Api>("api");
+var nextJsApp = builder.AddJavaScriptApp("nextjs-app", "../Middagsklok.NextJsApp", "dev")
+    .WithReference(apiService);
+
 builder.Build().Run();
